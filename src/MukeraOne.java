1b6e37649af276f84004b010fd9f5d46d6ddcb27
ammoki <122054042+ammoki@users.noreply.github.com>
2023-10-23 18:37:29 -0400

    This is my first commit

---
+++ src/MukeraOne.java
@@ -0,0 +1,8 @@
+public class MukeraOne {
+    public static void main(String[] args) {
+        getName("Kidist");
+    }
+    public static void getName(String name){
+        System.out.println(name + " Welcome to mukera one world!");
+    }
+}
